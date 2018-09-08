943b4c127382d926913f9af09be419b172726650
Chribtoper <xcstolfax@gmail.com>
2018-09-08 03:24:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+i = 0
+number = ""
+name = ""
+while i < name_hash.length
+  if name_hash[i][0] < number
+    number = name_hash[i][0]
+    name = name_hash[i]
+  end
+  name 
 end
