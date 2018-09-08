26791c445d78b42ed7f62e2625229553744d1ea8
Chribtoper <xcstolfax@gmail.com>
2018-09-08 03:54:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 num = nil
 final_name = nil
 name_hash.collect do |name, i|
-  if (num == nil || num <= i)
+  if (num == nil || num > i)
     final_name = name
     num = i 
   end
